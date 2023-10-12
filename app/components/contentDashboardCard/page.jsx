497358b800aaaeb22141c07d96563764f4ea6ef7
Lokesh <jhalokesh187@gmail.com>
2023-10-12 13:54:39 +0530

    contentManagement-dashboard-fixed

--- app/components/contentDashboardCard/page.jsx
+++ app/components/contentDashboardCard/page.jsx
@@ -8,15 +8,256 @@ import {
     Button,
     Input,
     Textarea,
+    Chip,
+    Tabs,
+    TabsHeader,
+    Tab,
+    Avatar,
+    IconButton,
+    Tooltip,
 } from "@material-tailwind/react";
 import { SortableTable } from "../dataTable/page";
-import { ContentManagementTable } from "../contenManagementTabled/page";
+import { faL } from "@fortawesome/free-solid-svg-icons";
+import {
+    MagnifyingGlassIcon,
+    ChevronUpDownIcon,
+} from "@heroicons/react/24/outline";
+import { PencilIcon, UserPlusIcon } from "@heroicons/react/24/solid";
+
+
+const TABS = [
+    {
+        label: "All",
+        value: "all",
+    },
+    {
+        label: "Monitored",
+        value: "monitored",
+    },
+    {
+        label: "Unmonitored",
+        value: "unmonitored",
+    },
+];
+
+const TABLE_HEAD = ["Member", "Function", "Status", "Employed", ""];
+
+const TABLE_ROWS = [
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-3.jpg",
+        name: "John Michael",
+        email: "john@creative-tim.com",
+        job: "Manager",
+        org: "Organization",
+        online: true,
+        date: "23/04/18",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-2.jpg",
+        name: "Alexa Liras",
+        email: "alexa@creative-tim.com",
+        job: "Programator",
+        org: "Developer",
+        online: false,
+        date: "23/04/18",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-1.jpg",
+        name: "Laurent Perrier",
+        email: "laurent@creative-tim.com",
+        job: "Executive",
+        org: "Projects",
+        online: false,
+        date: "19/09/17",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-4.jpg",
+        name: "Michael Levi",
+        email: "michael@creative-tim.com",
+        job: "Programator",
+        org: "Developer",
+        online: true,
+        date: "24/12/08",
+    },
+    {
+        img: "https://demos.creative-tim.com/test/corporate-ui-dashboard/assets/img/team-5.jpg",
+        name: "Richard Gran",
+        email: "richard@creative-tim.com",
+        job: "Manager",
+        org: "Executive",
+        online: false,
+        date: "04/10/21",
+    },
+];
+
+function ContentManagementTable() {
+    return (
+        <Card className="h-full w-full" shadow={false}>
+            <CardHeader floated={false} shadow={false} className="rounded-none">
+                <div className="mb-8 flex items-center justify-between gap-8">
+                    <div>
+                        <Typography variant="h5" color="blue-gray">
+                            Video list
+                        </Typography>
+                        <Typography color="gray" className="mt-1 font-normal">
+                            See information about all videos.
+                        </Typography>
+                    </div>
+                    <div className="flex shrink-0 flex-col gap-2 sm:flex-row">
+                        <Button variant="outlined" color="light-blue" size="sm">
+                            view all
+                        </Button>
+                        <Button className="flex items-center gap-3" color="light-blue" size="sm">
+                            {/* <UserPlusIcon strokeWidth={2} className="h-4 w-4" />  */}
+                            Add Videos
+                        </Button>
+                    </div>
+                </div>
+                <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
+                    <Tabs value="all" className="w-full md:w-max">
+                        <TabsHeader>
+                            {TABS.map(({ label, value }) => (
+                                <Tab key={value} value={value}>
+                                    &nbsp;&nbsp;{label}&nbsp;&nbsp;
+                                </Tab>
+                            ))}
+                        </TabsHeader>
+                    </Tabs>
+                    <div className="w-full md:w-72">
+                        <Input
+                            label="Search"
+                            icon={<MagnifyingGlassIcon className="h-5 w-5" />}
+                        />
+                    </div>
+                </div>
+            </CardHeader>
+            <CardBody className="overflow-scroll px-0">
+                <table className="mt-4 w-full min-w-max table-auto text-left">
+                    <thead>
+                        <tr>
+                            {TABLE_HEAD.map((head, index) => (
+                                <th
+                                    key={head}
+                                    className="cursor-pointer border-y border-blue-gray-100 bg-blue-gray-50/50 p-4 transition-colors hover:bg-blue-gray-50"
+                                >
+                                    <Typography
+                                        variant="small"
+                                        color="blue-gray"
+                                        className="flex items-center justify-between gap-2 font-normal leading-none opacity-70"
+                                    >
+                                        {head}{" "}
+                                        {index !== TABLE_HEAD.length - 1 && (
+                                            <ChevronUpDownIcon strokeWidth={2} className="h-4 w-4" />
+                                        )}
+                                    </Typography>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {TABLE_ROWS.map(
+                            ({ img, name, email, job, org, online, date }, index) => {
+                                const isLast = index === TABLE_ROWS.length - 1;
+                                const classes = isLast
+                                    ? "p-4"
+                                    : "p-4 border-b border-blue-gray-50";
+
+                                return (
+                                    <tr key={name}>
+                                        <td className={classes}>
+                                            <div className="flex items-center gap-3">
+                                                <Avatar src={img} alt={name} variant="square" className="h-20" size="xxl" />
+                                                <div className="flex flex-col">
+                                                    <Typography
+                                                        variant="small"
+                                                        color="blue-gray"
+                                                        className="font-normal"
+                                                    >
+                                                        {name}
+                                                    </Typography>
+                                                    <Typography
+                                                        variant="small"
+                                                        color="blue-gray"
+                                                        className="font-normal opacity-70"
+                                                    >
+                                                        {email}
+                                                    </Typography>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td className={classes}>
+                                            <div className="flex flex-col">
+                                                <Typography
+                                                    variant="small"
+                                                    color="blue-gray"
+                                                    className="font-normal"
+                                                >
+                                                    {job}
+                                                </Typography>
+                                                <Typography
+                                                    variant="small"
+                                                    color="blue-gray"
+                                                    className="font-normal opacity-70"
+                                                >
+                                                    {org}
+                                                </Typography>
+                                            </div>
+                                        </td>
+                                        <td className={classes}>
+                                            <div className="w-max">
+                                                <Chip
+                                                    variant="ghost"
+                                                    size="sm"
+                                                    value={online ? "online" : "offline"}
+                                                    color={online ? "green" : "blue-gray"}
+                                                />
+                                            </div>
+                                        </td>
+                                        <td className={classes}>
+                                            <Typography
+                                                variant="small"
+                                                color="blue-gray"
+                                                className="font-normal"
+                                            >
+                                                {date}
+                                            </Typography>
+                                        </td>
+                                        <td className={classes}>
+                                            <Tooltip content="Edit User">
+                                                <IconButton variant="text">
+                                                    <PencilIcon className="h-4 w-4" />
+                                                </IconButton>
+                                            </Tooltip>
+                                        </td>
+                                    </tr>
+                                );
+                            },
+                        )}
+                    </tbody>
+                </table>
+            </CardBody>
+            <CardFooter className="flex items-center justify-between border-t border-blue-gray-50 p-4 pt-10">
+                <Typography variant="small" color="blue-gray" className="font-normal">
+                    Save all the changes from here...
+                </Typography>
+                <div className="flex gap-2">
+                    <Button variant="outlined" color="light-blue" size="sm">
+                        Cancel
+                    </Button>
+                    <Button color="light-blue" size="sm">
+                        Save
+                    </Button>
+                </div>
+            </CardFooter>
+        </Card>
+    );
+}
 
 export function ContentDashboardCard() {
     return (
         <div className="p-4 pt-10 bg-blue-gray-50/50" style={{ userSelect: 'none' }}>
             <Card className="w-full">
-                <div className="flex justify-between flex-col sm:flex-col md:flex-row lg:flex-row items-center">
+                <div className="flex justify-between flex-col sm:flex-col md:flex-row lg:flex-row gap-6 md:gap-3 lg:gap-3">
 
 
                     <CardHeader color="blue-gray" className="sm:2-11/12 sm:h-11/12 md:w-1/3 md:h-2/3 lg:w-1/3 lg:h-1/3">
@@ -28,103 +269,41 @@ export function ContentDashboardCard() {
 
                     </CardHeader>
 
-                    
-
-
-                    <div className="w-full sm:w-full md:w-2/3 lg:w-[2/3] p-10 flex flex-col gap-6">
-
-
-                        <div className="flex flex-col md:flex-row lg:flex-row justify-between w-full gap-6 md:gap-3 lg:gap-3 ">
-
-                            <div className="w-full md:w-3/4 lg:w-3/4">
-
-                                <Input variant="standard" className="min-w-100px" label="Title" />
-
+                    <div className="w-full md:w-4/6 lg:w-4/6 flex items-center justify-centers md:pl-0 lg:pl-0 pl-6 pr-6">
+                        <div className="flex flex-col w-full gap-6">
+                            <div className="w-full ">
+                                <Input variant="standard" className="" label="Title" />
                             </div>
-
-
-                            <div className="w-full md:w-2/4 lg:w-2/4">
-
-                                <Input size="sm" variant="standard" className="" label="Price" />
-
+                            <div className="w-full">
+                                <Input size="" variant="standard" className="" label="Price" />
                             </div>
-
                         </div>
-
-                        <div className="">
-
-                            <Textarea variant="standard" label="Description" />
-
-                        </div>
-
                     </div>
-
-
-
-
 
                 </div>
 
-                {/* <div className="flex">
-
-                    <div className="w-2/6">
-                        <img
-                            className="w-11/12 h-fit -mt-6 mx-4 rounded-lg object-cover object-center shadow-lg shadow-blue-gray-500/40"
-                            src="https://images.unsplash.com/photo-1682407186023-12c70a4a35e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"
-                            alt="nature image"
-                        />
-                    </div>
-
-                    <div className="flex flex-col w-4/6 p-4">
-
-                        <div className="flex flex-row gap-3 w-full">
-
-                            <div className="w-3/4">
-
-                                <Input variant="standard" label="Title" />
-                                
-                            </div>
-
-                            <div className="w-1/4">
-
-                                <Input variant="standard" label="Price" />
-
-                            </div>
-
-                        </div>
-
-                        <div className="mt-10 h-full">
-
-                            <Textarea variant="standard" label="Description" />
-
-                        </div>
-
-                    </div>
-
-                </div> */}
-
                 <CardBody>
 
+                    <div className="w-full mb-6">
+                        <Textarea variant="standard" className="h-[10rem]" label="Description" />
+                    </div>
 
                     <ContentManagementTable />
 
                 </CardBody>
 
-                {/* <CardBody>
-                <Typography variant="h5" color="blue-gray" className="mb-2">
-                    UI/UX Review Check
-                </Typography>
-                <Typography>
-                    The place is close to Barceloneta Beach and bus stop just 2 min by
-                    walk and near to &quot;Naviglio&quot; where you can enjoy the main
-                    night life in Barcelona.
-                </Typography>
-                </CardBody>
-                <CardFooter className="pt-0 flex justify-between items-center">
-                <Typography variant="h3" color="blue-gray" className="">
-                    $199
-                </Typography>
-                <Button color="light-blue">Read More</Button>
+                {/* <CardFooter className="pt-0 flex justify-between items-center">
+                    <Typography variant="h3" color="blue-gray" className="">
+                       Save all the changes from here...
+                    </Typography>
+                    <div className="flex gap-2">
+                        <Button variant="outlined" color="light-blue" size="sm">
+                            Cancel
+                        </Button>
+                        <Button color="light-blue" size="sm">
+                            Save
+                        </Button>
+                    </div>
                 </CardFooter> */}
             </Card>
         </div>
